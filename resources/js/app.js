c70db012a87f5ec48f8f220c7c1992b5a33aebb8
kentaueno <kentaueno1995@icloud.com>
2025-05-13 21:28:41 +0900

    added filterComponent

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import './bootstrap';
-import './components/taskCard';
+import taskCard from './components/taskCard';
+import filterComponent from './components/filter';
 import Alpine from 'alpinejs';
+
+Alpine.data("taskCard", taskCard);
+Alpine.data("filterComponent", filterComponent);
 window.Alpine = Alpine;
 Alpine.start();
