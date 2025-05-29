89b60c8eb3b8bb4af843c8c01be081d937aae117
kentaueno <kentaueno1995@icloud.com>
2025-05-29 20:18:18 +0900

    Added variables that can be used in blade

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Task;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $now = Carbon::now();
+        $tomorrow = Carbon::now()->addDay();
 
-        $tasks = Task::where('user_id', auth()->id())->get();
+        $tasks = Task::where('user_id', auth()->id())->where('due_date', '>=', $now)->orderBy('due_date', 'asc')->get();
 
-        $unfinishedCount = $tasks->where('status', '!=', 'completed')->count();
+        $unfinishedCount = Task::where('user_id', auth()->id())->where('status', '!=', 'completed')->where('due_date', '>=', $now)->count();
 
-        return view('auth.dashboard', compact('tasks', 'unfinishedCount'));
+        $urgentTasksCount = Task::where('user_id', auth()->id())->where('due_date', '>=', $now)->where('due_date', '<=', $tomorrow)->where('status', '!=', 'completed')->count();
 
+        return view('auth.dashboard', compact('tasks', 'unfinishedCount', 'urgentTasksCount', 'now', 'tomorrow'));
     }
 }
