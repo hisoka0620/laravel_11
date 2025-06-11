a14d2ac993bda43371daf128f600b93000b282fb
kentaueno <kentaueno1995@icloud.com>
2025-06-11 21:03:48 +0900

    Changed task card UI (mainly colors)

--- resources/js/components/taskCard.js
+++ resources/js/components/taskCard.js
@@ -27,32 +27,56 @@ export default (task, csrfToken) => ({
     get toggleLabel() {
         return this.isCompleted ? "Mark as incomplete" : "Mark as completed";
     },
+    get borderClass() {
+        const now = new Date();
+        const tomorrow = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+        const dueDate = new Date(this.dueDate);
+        if (dueDate >= now && dueDate < tomorrow && !this.isCompleted) return "border-yellow-400";
+        if (dueDate < now && !this.isCompleted && this.dueDate != null) return "border-red-400";
+        if (this.isCompleted) return "border-green-400 opacity-50";
+        return "border-blue-400";
+    },
     get statusClass() {
         switch (this.status) {
-            case "completed": return "bg-green-100 text-green-600";
-            case "in_progress": return "bg-yellow-100 text-yellow-600";
-            case "pending": return "bg-orange-100 text-orange-600";
-            default: return "bg-gray-100 text-gray-600";
+            case "completed":
+                return "bg-green-100 text-green-600";
+            case "in_progress":
+                return "bg-orange-100 text-orange-600";
+            case "pending":
+                return "bg-purple-100 text-purple-600";
+            default:
+                return "bg-gray-100 text-gray-600";
         }
     },
     get priorityClass() {
         switch (this.priority) {
-            case "low": return "border-green-500 bg-green-100 text-green-600";
-            case "medium": return "border-blue-500 bg-blue-100 text-blue-600";
-            case "high": return "border-red-500 bg-red-100 text-red-600";
-            default: return "border-gray-500 bg-gray-100 text-gray-600";
+            case "low":
+                return "border-l-4 border-lime-500 bg-lime-100 text-lime-600";
+            case "medium":
+                return "border-l-4 border-cyan-500 bg-cyan-100 text-cyan-600";
+            case "high":
+                return "border-l-4 border-red-500 bg-red-100 text-red-600";
+            default:
+                return "border-l-4 border-gray-500 bg-gray-100 text-gray-600";
         }
     },
     get dueDateClass() {
         const now = new Date();
         const tomorrow = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+        if (!this.dueDate) {
+            return "italic text-gray-600 bg-gray-100";
+        }
         const dueDate = new Date(this.dueDate);
+        if (isNaN(dueDate.getTime())) {
+            // If it is an invalid date (e.g., cannot be parsed)
+            return "italic text-gray-600 bg-gray-100";
+        }
         if (now > dueDate) {
             return "text-red-600 bg-red-100";
         } else if (dueDate >= now && dueDate < tomorrow) {
             return "text-yellow-600 bg-yellow-100";
         } else {
-            return "text-slate-600";
+            return "text-slate-600 bg-slate-100";
         }
     },
     get isTasksDueSoon() {
