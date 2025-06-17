9548198db93910a85b4e5a48c831c66f2ec2a486
kentaueno <kentaueno1995@icloud.com>
2025-06-17 23:42:06 +0900

    Move auth route to auth.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TaskController;
-use App\Http\Controllers\PasswordResetLinkController;
-use App\Http\Controllers\NewPasswordController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,31 +10,9 @@ Route::get('/', function () {
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
 
-/*-- Auth Route --*/
-
-Route::middleware('guest')->group(function () {
-    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
-
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
-
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
-
-    Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
-
-    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
-
-    Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
-
-    Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.update');
-});
-
 /*-- Task Route --*/
 
 Route::middleware(['auth'])->group(function () {
-
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
     Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
@@ -53,3 +28,5 @@ Route::middleware(['auth'])->group(function () {
 
     Route::patch('/tasks/{task}/toggle-status', [TaskController::class, 'toggleStatusAjax'])->name('tasks.toggleStatusAjax');
 });
+
+require base_path('routes/auth.php');
