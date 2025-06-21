5c0a62424385405ab21a16f8096ec67ff110b5b8
kentaueno <kentaueno1995@icloud.com>
2025-06-21 23:48:29 +0900

    Create common queries using scope and minor modifications

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,32 +3,39 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
-use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $userId = auth()->id();
-        $now = Carbon::now();
+        $now = now();
         $tomorrow = $now->copy()->addDay();
 
         $baseQuery = Task::where('user_id', $userId);
 
         $tasks = $baseQuery->clone()->orderBy('due_date', 'asc')->get();
 
-        $ongoingTasks = $baseQuery->clone()->where(function ($query) use ($now) {
-            $query->where('due_date', '>=', $now)->orWhereNull('due_date');
-        });
+        $ongoingTasks = $baseQuery->clone()->ongoingTasks()->get();
 
         $completedCount = $baseQuery->clone()->where('status', '=', 'completed')->count();
 
         $expiredCount = $baseQuery->clone()->where('due_date', '<', $now)->where('status', '!=', 'completed')->count();
 
-        $unfinishedCount = $ongoingTasks->clone()->where('status', '!=', 'completed')->count();
+        $unfinishedCount = $baseQuery->clone()->ongoingTasks()->count();
 
-        $urgentTasksCount = $ongoingTasks->clone()->where('due_date', '<', $tomorrow)->where('status', '!=', 'completed')->count();
+        $urgentTasksCount = $baseQuery->clone()->ongoingTasks()->where('due_date', '<', $tomorrow)->count();
 
-        return view('auth.dashboard', compact('tasks', 'unfinishedCount', 'urgentTasksCount', 'expiredCount', 'completedCount', 'now'));
+        return view(
+            'auth.dashboard',
+            compact(
+                'tasks',
+                'unfinishedCount',
+                'urgentTasksCount',
+                'expiredCount',
+                'completedCount',
+                'ongoingTasks',
+            )
+        );
     }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Task extends Model
 {
@@ -25,5 +26,12 @@ class Task extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeOngoingTasks(Builder $query)
+    {
+        return $query->where('status', '!=', 'completed')->where(function ($query) {
+            $query->whereNull('due_date')->orWhere('due_date', '>=', now());
+        });
     }
 }
